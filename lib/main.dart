a24febf4b57c0b7e71bf50b9cfdbadfd9a9f01c4
SinCara-soufiane <sfnweb2@gmail.com>
2024-11-28 21:04:29 +0100

    Implement Login Screen with Login Logic
    - Extracted login logic from the Sign-Up feature branch, which was committed incorrectly.

--- lib/main.dart
+++ lib/main.dart
@@ -25,6 +25,7 @@ class OpExpenseApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
+      
       designSize: const Size(375, 812),
       minTextAdapt: true,
       child: MaterialApp(
