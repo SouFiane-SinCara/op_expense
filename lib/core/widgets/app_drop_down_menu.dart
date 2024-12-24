77ecbd91c2e4189164e65652fa4e0e4f98072e7d
SinCara-soufiane <sfnweb2@gmail.com>
2024-12-24 12:17:52 +0100

    Refactor AddTransactionScreen  with  Add attachments and repeat transaction functionalities
    - Add iOS image picker configuration in Info.plist
    
    - Implement attachment functionality with gallery/camera options
    
    - Improve AppDropdownMenu design and usability
    
     - Add arrow-up icon
    
     - Remove trailingIconColor property
    
    - Add repeat transaction feature
    
    - Refactor AddTransactionScreen architecture

--- lib/core/widgets/app_drop_down_menu.dart
+++ lib/core/widgets/app_drop_down_menu.dart
@@ -4,59 +4,89 @@ import 'package:flutter_svg/flutter_svg.dart';
 import 'package:op_expense/core/theme/app_colors.dart';
 import 'package:op_expense/core/theme/text_styles.dart';
 
-class AppDropDownMenu extends StatelessWidget {
+class AppDropDownMenu extends StatefulWidget {
   final Function(dynamic)? onSelected;
   final List<DropdownMenuEntry<Object>> dropdownMenuEntries;
   final Object selectedType;
-  final Color trailingIconColor;
+
   final String? hintText;
-  const AppDropDownMenu(
-      {super.key,
-      required this.onSelected,
-        this.hintText,
-      required this.selectedType,
-      required this.dropdownMenuEntries,
-      required this.trailingIconColor});
+
+  const AppDropDownMenu({
+    super.key,
+    required this.onSelected,
+    this.hintText,
+    required this.selectedType,
+    required this.dropdownMenuEntries,
+  });
 
   @override
+  State<AppDropDownMenu> createState() => _AppDropDownMenuState();
+}
+
+class _AppDropDownMenuState extends State<AppDropDownMenu> {
+  bool itemSelected = false;
+  @override
   Widget build(BuildContext context) {
-    return DropdownMenu(
-      menuHeight: 200.h,
-      width: double.infinity,
-      trailingIcon: SvgPicture.asset(
-        'lib/core/assets/icons/Magicons/Glyph/Arrow/arrow-down-2.svg',
-        colorFilter: ColorFilter.mode(trailingIconColor, BlendMode.srcIn),
-      ),
-      hintText: hintText ?? 'Select',
-      menuStyle: const MenuStyle(
-        backgroundColor: WidgetStatePropertyAll(
-          AppColors.violet20,
-        ),
-      ),
-      textStyle: TextStyles.w500Dark.copyWith(fontSize: 18.sp),
-      inputDecorationTheme: InputDecorationTheme(
-        // ignore: prefer_const_constructors
-        outlineBorder: BorderSide(
-          width: 1,
-          color: AppColors.light60,
-        ),
-        enabledBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(15.r),
-          borderSide: const BorderSide(
-            width: 1,
-            color: AppColors.light60,
-          ),
-        ),
-        border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(15.r),
-          borderSide: const BorderSide(
-            width: 1,
-            color: AppColors.light60,
+    return Theme(
+      data: Theme.of(context).copyWith(
+        inputDecorationTheme: InputDecorationTheme(
+          hintStyle: TextStyles.w500Light20.copyWith(
+            fontSize: 18.sp,
+            color: AppColors.light20, // Color of the hint text
           ),
         ),
       ),
-      dropdownMenuEntries: dropdownMenuEntries,
-      onSelected: onSelected,
+      child: DropdownMenu(
+        
+        menuHeight: 200.h,
+        selectedTrailingIcon: SvgPicture.asset(
+          'lib/core/assets/icons/Magicons/Glyph/Arrow/arrow-up.svg',
+          colorFilter: ColorFilter.mode(
+              itemSelected ? AppColors.dark : AppColors.light20,
+              BlendMode.srcIn),
+        ),
+        width: double.infinity,
+        trailingIcon: SvgPicture.asset(
+          'lib/core/assets/icons/Magicons/Glyph/Arrow/arrow-down-2.svg',
+          colorFilter: ColorFilter.mode(
+              itemSelected ? AppColors.dark : AppColors.light20,
+              BlendMode.srcIn),
+        ),
+        hintText: widget.hintText ?? 'Select',
+        menuStyle: const MenuStyle(
+          backgroundColor: WidgetStatePropertyAll(
+            AppColors.violet20,
+          ),
+        ),
+        textStyle: TextStyles.w500Dark
+            .copyWith(fontSize: 18.sp, overflow: TextOverflow.clip),
+        inputDecorationTheme: InputDecorationTheme(
+          outlineBorder: const BorderSide(
+            width: 1,
+            color: AppColors.light60,
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(15.r),
+            borderSide: const BorderSide(
+              width: 1,
+              color: AppColors.light60,
+            ),
+          ),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(15.r),
+            borderSide: const BorderSide(
+              width: 1,
+              color: AppColors.light60,
+            ),
+          ),
+        ),
+        dropdownMenuEntries: widget.dropdownMenuEntries,
+        onSelected: (value) {
+          widget.onSelected!(value);
+          itemSelected = true;
+          setState(() {});
+        },
+      ),
     );
   }
 }
